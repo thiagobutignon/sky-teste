8414a30d6c4dd458171c654985a81f587e8505c4
Thiago Butignon <thiagobutignon@gmail.com>
2021-02-08 20:58:43 -0300

    test: create mockAvailableChannel

---
+++ src/domain/test/movies-result-models/mock-available-channel.ts
@@ -0,0 +1,16 @@
+import { AvailableChannel } from '@/domain/models/movies-result-models'
+import faker from 'faker'
+
+export const mockAvailableChannel = (): AvailableChannel => {
+  return {
+    hashKey: faker.random.uuid(),
+    title: faker.random.words(),
+    shortDescription: faker.random.words(),
+    isElegible: faker.random.boolean(),
+    number: faker.random.number(),
+    type: faker.random.number(),
+    highlights: {
+      items: []
+    }
+  }
+}
