fa57d04360273160fa06ad4668737c3ff4edb7f6
Thiago Butignon <thiagobutignon@gmail.com>
2021-02-09 12:59:08 -0300

    test: ensure AxiosHttpClient call axios with correct values

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,43 @@
+import { AxiosHttpClient } from '@/infra/http'
+import { mockAxios } from '@/infra/test'
+import { mockHttpRequest } from '@/data/test'
+
+import axios from 'axios'
+
+jest.mock('axios')
+
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
+  return {
+    sut,
+    mockedAxios
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct values', async () => {
+    const request = mockHttpRequest()
+    const { sut, mockedAxios } = makeSut()
+
+    await sut.request(request)
+
+    expect(mockedAxios.request).toHaveBeenCalledWith({
+      url: request.url,
+      data: request.body,
+      headers: {
+        ...request.headers,
+        AccessControlAllowOrigin: '*',
+        AccessControlAllowHeaders: 'Origin, Content-Type, Authorization',
+        AccessControlAllowMethods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        ContentType: 'application/json'
+      },
+      method: request.method
+    })
+  })
+})
