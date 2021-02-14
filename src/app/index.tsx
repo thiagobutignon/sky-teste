541a019259364152fb2cf20335631e31fe33f4f5
Thiago Butignon <thiagobutignon@gmail.com>
2021-02-14 18:50:00 -0300

    feat: create Home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import 'antd/dist/antd.css'
-import { Card } from 'antd'
+
+import { Home } from '@/presentation/pages'
 ReactDOM.render(
-  <Card title="Card title" bordered={false} style={{ width: 300 }}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
+  <Home/>
   , document.getElementById('main'))
---
+++ src/presentation/pages/home-page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout } from '@/presentation/components/layout'
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Teste</h1>
+      </Layout>
+    </>
+  )
+}
+
+export default HomePage
